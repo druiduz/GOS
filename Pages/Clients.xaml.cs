25f0f42476130b4440050b1c77e577fc2ade7176
Bensanssous <sebbar.sabrina@gmail.com>
2013-04-29 15:20:43 +0200

    page client

---
+++ Pages/Clients.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GOS.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour Clients.xaml
+    /// </summary>
+    public partial class Clients : Page
+    {
+        public Clients()
+        {
+            InitializeComponent();
+        }
+
+        private void ListeClients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
